f7cb75e9051f91b8913ec07fcbacd7e2a4cba151
Noothan am <100135382+Noothan-am@users.noreply.github.com>
2025-05-21 22:38:38 +0530

    feat(context): set up React Context API for app state

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { PageLayout } from "./components/layout/PageLayout";
 import { HomePage } from "./pages/HomePage";
 import { NewPatientPage } from "./pages/NewPatientPage";
+import { PGliteProvider } from "./context/PGliteContext";
 
 function App() {
   return (
-    <Router>
-      <PageLayout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/new" element={<NewPatientPage />} />
-        </Routes>
-      </PageLayout>
-    </Router>
+    <PGliteProvider>
+      <Router>
+        <PageLayout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/new" element={<NewPatientPage />} />
+          </Routes>
+        </PageLayout>
+      </Router>
+    </PGliteProvider>
   );
 }
 
